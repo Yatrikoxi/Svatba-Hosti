e0be5f9a6cd04a39023687dfff6847a74ee5fa0c
Yatrikoxi <tomas_novak@centrum.cz>
2023-07-26 13:01:57 +0200

    add form and list

--- src/components/Amount.tsx
+++ src/components/Amount.tsx
@@ -10,6 +10,22 @@ const Amount = () => {
       family: true,
       sleep: false,
     },
+    {
+      id: 2,
+      name: "Novákovic Pepík",
+      number: 2,
+      side: "H",
+      family: true,
+      sleep: false,
+    },
+    {
+      id: 3,
+      name: "Novákovic Pepík",
+      number: 2,
+      side: "H",
+      family: true,
+      sleep: false,
+    },
   ];
   // const total = person.reduce((acc, number) => acc + number.value, 0);
   const [number, setNumber] = useState(2);
@@ -19,19 +35,23 @@ const Amount = () => {
         <p className='h1'>Celkový počet:</p>
         <p className='h1'>{number}</p>
       </div>
-      <ul className='list-group d-flex flex-column'>
-        {person.map(({ id, name, number }) => (
-          <li
-            className='list-group-item d-flex justify-content-between'
-            key={id}
-          >
-            <p className='m-0 py-2'>
-              {name}, počet: {number}
-            </p>
-            <button className='btn btn-danger'>Smazat</button>
-          </li>
+      <table className='table table-bordered'>
+        <tr>
+          <th scope='col'>Jméno</th>
+          <th scope='col'>Počet</th>
+          <th scope='col'>R/K</th>
+          <th scope='col'>Spaní</th>
+        </tr>
+        {person.map(({ id, name, side, number, sleep }) => (
+          <tr key={id}>
+            <td>{name}</td>
+            <td>{number}</td>
+            <td>{side}</td>
+            <td>{sleep}</td>
+            <button className='btn btn-primary'>Smazat</button>
+          </tr>
         ))}
-      </ul>
+      </table>
     </>
   );
 };
